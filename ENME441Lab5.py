b471822d4716c1669c12191bd513a076e8a78625
balsh22 <balshw22@gmail.com>
2025-10-08 15:15:14 -0400

    Add files via upload

--- ENME441Lab5.py
+++ ENME441Lab5.py
@@ -1,13 +1,23 @@
 import RPi.GPIO as GPIO
 import math
 import time
+import threading
 GPIO.setmode(GPIO.BCM)
+pos_neg = 1
 
 pins = [14, 15, 18, 23, 24, 25, 8, 7, 12, 16]
 p = 25    # GPIO pin number
 f = 0.2     # frequency (Hz)
 basef = 500
 phi = math.pi/11
+
+in1 = 26
+GPIO.setup(in1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def callbackFunction():
+  pos_neg *= -1
+
+gpio.add_event_detect(in1, GPIO.RISING, callback=callbackFunction, bouncetime=100)
 
 pins2 = []
 for pin in pins:
@@ -20,7 +30,7 @@ try:
   while True:
     t = time.time() - start_time
     for n, pin in enumerate(pins2):
-      brightness = math.sin(2 * math.pi * f * t - (phi * n)) ** 2
+      brightness = math.sin(2 * math.pi * f * t - (phi * n * pos_neg)) ** 2
       pin.start(brightness * 100)
 
 except KeyboardInterrupt:   # stop gracefully on ctrl-C
