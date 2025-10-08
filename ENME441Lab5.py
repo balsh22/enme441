89c2b4833e79eb71cba374bb57c6a99d8da529d4
balsh22 <balshw22@gmail.com>
2025-10-08 14:55:12 -0400

    Add files via upload

--- ENME441Lab5.py
+++ ENME441Lab5.py
@@ -3,23 +3,30 @@ import math
 import time
 GPIO.setmode(GPIO.BCM)
 
+pins = [14, 15, 18, 23, 24, 25, 8, 7, 12, 16]
 p = 25    # GPIO pin number
 f = 0.2     # frequency (Hz)
 basef = 500
 phi = math.pi/11
 
-GPIO.setup(p, GPIO.OUT)
-pwm = GPIO.PWM(p, basef)        # create PWM object
+pins2 = []
+for pin in pins:
+  GPIO.setup(pin, GPIO.OUT)
+  pwm = GPIO.PWM(pin, basef)        # create PWM object
+  pins2.append(pwm)
 
 try:
   start_time = time.time()
   while True:
     t = time.time() - start_time
-    brightness = math.sin(2 * math.pi * f * t - phi) ** 2
-    pwm.start(brightness * 100)
+    for n, pin in enumerate(pins2):
+    brightness = math.sin(2 * math.pi * f * t - (phi * n)) ** 2
+    pins2.start(brightness * 100)
 
 except KeyboardInterrupt:   # stop gracefully on ctrl-C
   print('\nExiting')
 
-pwm.stop()
-GPIO.cleanup()
+finally:
+  for pin in pins2:
+    pwm.stop()
+  GPIO.cleanup()
